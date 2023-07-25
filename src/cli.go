e8e0238181d62133614f491a3dac9ad4f4c989bf
hiddenMedic <124312252+hiddenMedic@users.noreply.github.com>
2023-07-25 23:06:24 +0200

    fixed issue #-1

---
+++ src/cli.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/alecthomas/kong"
+)
+
+var (
+	// release variables
+	Version   string
+	Timestamp string
+	GitCommit string
+
+	// CLI
+	cli struct {
+		globals
+
+		// flags
+		Query     string `type:"string" default:"${query_string}" env:"BRZAGUZA_QUERY" help:"Query string used for search"`
+		Log       string `type:"path" default:"${log_file}" env:"BRZAGUZA_LOG_FILE" help:"Log file path"`
+		Verbosity int    `type:"counter" default:"0" short:"v" env:"BRZAGUZA_VERBOSITY" help:"Log level verbosity"`
+	}
+)
+
+type globals struct {
+	Version versionFlag `name:"version" help:"Print version information and quit"`
+}
+
+type versionFlag string
+
+func (v versionFlag) Decode(ctx *kong.DecodeContext) error { return nil }
+func (v versionFlag) IsBool() bool                         { return true }
+func (v versionFlag) BeforeApply(app *kong.Kong, vars kong.Vars) error {
+	fmt.Println(vars["version"])
+	app.Exit(0)
+	return nil
+}
+
+func setupCli() {
+	ctx := kong.Parse(&cli,
+		kong.Name("brzaguza"),
+		kong.Description("Fastasst metasearch engine"),
+		kong.UsageOnError(),
+		kong.ConfigureHelp(kong.HelpOptions{
+			Summary: true,
+			Compact: true,
+		}),
+		kong.Vars{
+			"version":      fmt.Sprintf("%s (%s@%s)", Version, GitCommit, Timestamp),
+			"log_file":     "brzaguza.log",
+			"query_string": "cars for sale in Toronto, Canada",
+		},
+	)
+
+	if err := ctx.Validate(); err != nil {
+		fmt.Println("Failed parsing cli:", err)
+		os.Exit(1)
+	}
+}
