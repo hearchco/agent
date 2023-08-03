197ecd41ce3ef17321ddee4be611b82d10dd0113
hiddenMedic <124312252+hiddenMedic@users.noreply.github.com>
2023-08-03 15:41:33 +0200

    better SE selection with enum

--- src/search/search.go
+++ src/search/search.go
@@ -14,6 +14,15 @@ import (
 	"github.com/tminaorg/brzaguza/src/structures"
 )
 
+type Engine int
+
+const (
+	Google Engine = iota
+	Mojeek
+	DuckDuckGo
+	Qwant
+)
+
 func PerformSearch(query string, maxPages int, visitPages bool) []structures.Result {
 	relay := structures.Relay{
 		ResultMap:         make(map[string]*structures.Result),
@@ -28,35 +37,8 @@ func PerformSearch(query string, maxPages int, visitPages bool) []structures.Res
 	query = url.QueryEscape(query)
 
 	var worker conc.WaitGroup
-
-	worker.Go(func() {
-		err := google.Search(context.Background(), query, &relay, &options)
-		if err != nil {
-			log.Error().Err(err).Msg("Failed searching google.com")
-		}
-	})
-
-	worker.Go(func() {
-		err := duckduckgo.Search(context.Background(), query, &relay, &options)
-		if err != nil {
-			log.Error().Err(err).Msg("Failed searching lite.duckduckgo.com")
-		}
-	})
-
-	worker.Go(func() {
-		err := mojeek.Search(context.Background(), query, &relay, &options)
-		if err != nil {
-			log.Error().Err(err).Msg("Failed searching mojeek.com")
-		}
-	})
-
-	worker.Go(func() {
-		err := qwant.Search(context.Background(), query, &relay, &options)
-		if err != nil {
-			log.Error().Err(err).Msg("Failed searching qwant.com")
-		}
-	})
-
+	var toSearch []Engine = []Engine{Google, Mojeek, DuckDuckGo}
+	runEngines(toSearch, query, &worker, &relay, &options)
 	worker.Wait()
 
 	var results []structures.Result = make([]structures.Result, 0, len(relay.ResultMap))
@@ -72,4 +54,39 @@ func PerformSearch(query string, maxPages int, visitPages bool) []structures.Res
 	log.Debug().Msg("Done! Received All Engines!")
 
 	return results
+}
+
+func runEngines(toSearch []Engine, query string, worker *conc.WaitGroup, relay *structures.Relay, options *structures.Options) {
+	for _, eng := range toSearch {
+		switch Engine(eng) {
+		case Google:
+			worker.Go(func() {
+				err := google.Search(context.Background(), query, relay, options)
+				if err != nil {
+					log.Error().Err(err).Msg("Failed searching google.com")
+				}
+			})
+		case DuckDuckGo:
+			worker.Go(func() {
+				err := duckduckgo.Search(context.Background(), query, relay, options)
+				if err != nil {
+					log.Error().Err(err).Msg("Failed searching lite.duckduckgo.com")
+				}
+			})
+		case Mojeek:
+			worker.Go(func() {
+				err := mojeek.Search(context.Background(), query, relay, options)
+				if err != nil {
+					log.Error().Err(err).Msg("Failed searching mojeek.com")
+				}
+			})
+		case Qwant:
+			worker.Go(func() {
+				err := qwant.Search(context.Background(), query, relay, options)
+				if err != nil {
+					log.Error().Err(err).Msg("Failed searching qwant.com")
+				}
+			})
+		}
+	}
 }
