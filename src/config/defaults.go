32faade42af7c2902827ec07c2e7cb4ace747558
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2023-09-03 15:02:11 +0200

    Rebasing to logger conflicts

--- src/config/defaults.go
+++ src/config/defaults.go
@@ -4,6 +4,7 @@ import "github.com/tminaorg/brzaguza/src/engines"
 
 const DefaultLocale string = "en-US"
 
+<<<<<<< HEAD
 func New() *Config {
 	return &Config{
 		Engines: map[string]Engine{
@@ -69,4 +70,63 @@ func New() *Config {
 			},
 		},
 	}
+=======
+var DefaultConfig Config = Config{
+	Engines: map[string]Engine{
+		engines.Bing.ToLower(): {
+			Enabled: true,
+			Settings: Settings{
+				Shortcut: "bi",
+			},
+		},
+		engines.Brave.ToLower(): {
+			Enabled: true,
+			Settings: Settings{
+				Shortcut: "br",
+			},
+		},
+		engines.DuckDuckGo.ToLower(): {
+			Enabled: true,
+			Settings: Settings{
+				Shortcut: "ddg",
+			},
+		},
+		engines.Etools.ToLower(): {
+			Enabled: true,
+			Settings: Settings{
+				Shortcut: "ets",
+			},
+		},
+		engines.Google.ToLower(): {
+			Enabled: true,
+			Settings: Settings{
+				Shortcut: "go",
+			},
+		},
+		engines.Mojeek.ToLower(): {
+			Enabled: true,
+			Settings: Settings{
+				Shortcut: "mjk",
+			},
+		},
+		engines.Qwant.ToLower(): {
+			Enabled: true,
+			Settings: Settings{
+				Shortcut: "qw",
+			},
+		},
+		engines.Startpage.ToLower(): {
+			Enabled: true,
+			Settings: Settings{
+				Shortcut: "sp",
+			},
+		},
+		engines.Swisscows.ToLower(): {
+			Enabled: true,
+			Settings: Settings{
+				Shortcut: "sc",
+			},
+		},
+	},
+>>>>>>> 75bb49c (Using correct formating inside the code, while loading is lowercase)
 }
