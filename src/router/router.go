027ae30444d71c3bebbf8153f2e4668b6944bc40
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2023-09-05 16:42:30 +0200

    Removed unnecessary itoa

--- src/router/router.go
+++ src/router/router.go
@@ -2,7 +2,6 @@ package router
 
 import (
 	"fmt"
-	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/tminaorg/brzaguza/src/config"
@@ -12,5 +11,5 @@ var router = gin.Default()
 
 func Setup(config *config.Config) {
 	Search(config)
-	router.Run(fmt.Sprintf(":%v", strconv.Itoa(config.Server.Port)))
+	router.Run(fmt.Sprintf(":%v", config.Server.Port))
 }
