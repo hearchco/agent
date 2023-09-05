5167da80d77afcd356220c0df798a25b7225e195
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2023-09-05 13:02:54 +0200

    Switch to go:generate Enumer

--- src/engines/name.go
+++ src/engines/name.go
@@ -4,7 +4,7 @@ import "strings"
 
 type Name int
 
-//go:generate stringer -type=Name
+//go:generate go run github.com/dmarkham/enumer -type=Name
 const (
 	Undefined Name = iota
 	Bing
@@ -24,35 +24,4 @@ const (
 
 func (n Name) ToLower() string {
 	return strings.ToLower(n.String())
-}
-
-func ConvertToName(s string) Name {
-	switch strings.ToLower(s) {
-	case Google.ToLower():
-		return Google
-	case Mojeek.ToLower():
-		return Mojeek
-	case DuckDuckGo.ToLower():
-		return DuckDuckGo
-	case Qwant.ToLower():
-		return Qwant
-	case Etools.ToLower():
-		return Etools
-	case Swisscows.ToLower():
-		return Swisscows
-	case Brave.ToLower():
-		return Brave
-	case Bing.ToLower():
-		return Bing
-	case Startpage.ToLower():
-		return Startpage
-	case Yahoo.ToLower():
-		return Yahoo
-	case Yandex.ToLower():
-		return Yandex
-	case Yep.ToLower():
-		return Yep
-	default:
-		return Undefined
-	}
 }
