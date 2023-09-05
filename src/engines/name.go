4abdaa485509906041feaa8affe51f8784156296
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2023-09-05 11:06:42 +0200

    Rebasing to main

---
+++ src/engines/name.go
@@ -0,0 +1,67 @@
+package engines
+
+import "strings"
+
+type Name int64
+
+const (
+	Undefined Name = iota
+	Bing
+	Brave
+	DuckDuckGo
+	Etools
+	Google
+	Mojeek
+	Presearch
+	Qwant
+	Startpage
+	Swisscows
+	Yandex
+	Yep
+	Yahoo
+)
+
+func (n Name) String() string {
+	switch n {
+	case Bing:
+		return "bing"
+	case Brave:
+		return "brave"
+	case DuckDuckGo:
+		return "duckduckgo"
+	case Etools:
+		return "etools"
+	case Google:
+		return "google"
+	case Mojeek:
+		return "mojeek"
+	case Presearch:
+		return "presearch"
+	case Qwant:
+		return "qwant"
+	case Startpage:
+		return "startpage"
+	case Swisscows:
+		return "swisscows"
+	case Yandex:
+		return "yandex"
+	case Yep:
+		return "yep"
+	case Yahoo:
+		return "yahoo"
+	default:
+		return "undefined"
+	}
+}
+
+func (n Name) ToUpper() string {
+	return strings.ToUpper(n.String())
+}
+
+func (n Name) ToLower() string {
+	return strings.ToLower(n.String())
+}
+
+func (n Name) Equals(s string) bool {
+	return n.ToUpper() == strings.ToUpper(s)
+}
