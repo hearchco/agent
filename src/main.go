00628b0fbf34df9cc73e1a0238ba1bf8cd123c7e
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2023-10-08 18:59:45 +0200

    refactored climode to package since main() was getting huge

--- src/main.go
+++ src/main.go
@@ -2,39 +2,21 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
 
 	"github.com/rs/zerolog/log"
-	"github.com/tminaorg/brzaguza/src/bucket/result"
 	"github.com/tminaorg/brzaguza/src/cache"
 	"github.com/tminaorg/brzaguza/src/cache/nocache"
 	"github.com/tminaorg/brzaguza/src/cache/pebble"
 	"github.com/tminaorg/brzaguza/src/cache/redis"
+	"github.com/tminaorg/brzaguza/src/climode"
 	"github.com/tminaorg/brzaguza/src/config"
-	"github.com/tminaorg/brzaguza/src/engines"
 	"github.com/tminaorg/brzaguza/src/logger"
 	"github.com/tminaorg/brzaguza/src/router"
-	"github.com/tminaorg/brzaguza/src/search"
 )
-
-// todo: this should be refactor to cliMode package
-func printResults(results []result.Result) {
-	fmt.Print("\n\tThe Search Results:\n\n")
-	for _, r := range results {
-		fmt.Printf("%v (%.2f) -----\n\t\"%v\"\n\t\"%v\"\n\t\"%v\"\n\t-", r.Rank, r.Score, r.Title, r.URL, r.Description)
-		for seInd := uint8(0); seInd < r.TimesReturned; seInd++ {
-			fmt.Printf("%v", r.EngineRanks[seInd].SearchEngine)
-			if seInd != r.TimesReturned-1 {
-				fmt.Print(", ")
-			}
-		}
-		fmt.Printf("\n")
-	}
-}
 
 func main() {
 	mainTimer := time.Now()
@@ -66,36 +48,7 @@ func main() {
 
 	// startup
 	if cli.Cli {
-		log.Info().
-			Str("query", cli.Query).
-			Str("max-pages", fmt.Sprintf("%v", cli.MaxPages)).
-			Str("visit", fmt.Sprintf("%v", cli.Visit)).
-			Msg("Started searching")
-
-		options := engines.Options{
-			MaxPages:   cli.MaxPages,
-			VisitPages: cli.Visit,
-		}
-
-		start := time.Now()
-
-		// todo: this should be refactor to cliMode package with ctx cancelling as well
-		var results []result.Result
-		db.Get(cli.Query, &results)
-		if results != nil {
-			log.Debug().Msgf("Found results for query (%v) in cache", cli.Query)
-		} else {
-			log.Debug().Msg("Nothing found in cache, doing a clean search")
-			results = search.PerformSearch(cli.Query, options, conf)
-			db.Set(cli.Query, results)
-		}
-
-		duration := time.Since(start)
-
-		if !cli.Silent {
-			printResults(results)
-		}
-		log.Info().Msgf("Found %v results in %vms", len(results), duration.Milliseconds())
+		climode.Run(cli.Query, cli.MaxPages, cli.Visit, cli.Silent, db, conf)
 	} else {
 		if rw, err := router.New(conf, cli.Verbosity); err != nil {
 			log.Error().Msgf("Failed creating a router: %v", err)
