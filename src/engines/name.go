8b84419c24bb0a3baccf700c2ab7a07a2d0fad27
hiddenMedic <124312252+hiddenMedic@users.noreply.github.com>
2024-01-26 19:13:14 +0100

    feat: added pretty names

--- src/engines/name.go
+++ src/engines/name.go
@@ -1,6 +1,9 @@
 package engines
 
-import "strings"
+import (
+	"log"
+	"strings"
+)
 
 type Name uint8
 
@@ -22,6 +25,39 @@ const (
 	YAHOO
 	YEP
 )
+
+func initPrettyName() {
+	// laid out like this instead of ["Undefined", "Bing", "Brave", ...] so the index doesn't matter
+	PrettyName = make([]string, len(_NameValues))
+	PrettyName[UNDEFINED] = "UNDEFINED"
+	PrettyName[BING] = "Bing"
+	PrettyName[BRAVE] = "Brave"
+	PrettyName[DUCKDUCKGO] = "DuckDuckGo"
+	PrettyName[ETOOLS] = "Etools"
+	PrettyName[GOOGLE] = "Google"
+	PrettyName[GOOGLESCHOLAR] = "GoogleScholar"
+	PrettyName[MOJEEK] = "Mojeek"
+	PrettyName[PRESEARCH] = "Presearch"
+	PrettyName[QWANT] = "Qwant"
+	PrettyName[STARTPAGE] = "Startpage"
+	PrettyName[SWISSCOWS] = "Swisscows"
+	PrettyName[YAHOO] = "Yahoo"
+	PrettyName[YEP] = "Yep"
+
+	// Check if all search engines have a pretty name set
+	for _, eng := range NameValues() {
+		if PrettyName[eng] == "" {
+			log.Fatalf("engines.init() (names.go): %v doesn't have a pretty name set.", eng)
+			// ^FATAL
+		}
+	}
+}
+
+func init() {
+	initPrettyName()
+}
+
+var PrettyName []string
 
 // Returns Engine Names without UNDEFINED
 func Names() []Name {
