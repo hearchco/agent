f689391ad2245bdab05e76bffdef2e9094486fcc
hiddenMedic <124312252+hiddenMedic@users.noreply.github.com>
2024-01-26 18:02:33 +0100

    fix: defaults refactor

--- src/engines/etools/options.go
+++ src/engines/etools/options.go
@@ -9,7 +9,7 @@ var Info engines.Info = engines.Info{
 	Name:           engines.ETOOLS,
 	URL:            "https://www.etools.ch/searchSubmit.do",
 	ResultsPerPage: 10,
-	Crawlers:       []engines.Name{engines.BING, engines.GOOGLE, engines.MOJEEK, engines.YANDEX},
+	Crawlers:       []engines.Name{engines.BING, engines.GOOGLE, engines.MOJEEK},
 }
 
 var dompaths engines.DOMPaths = engines.DOMPaths{
