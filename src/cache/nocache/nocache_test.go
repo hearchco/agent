05faa8ed711982426b3a74132f4577d29f33f107
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2024-02-04 20:02:34 +0100

    chore(cache): nocache tests

---
+++ src/cache/nocache/nocache_test.go
@@ -0,0 +1,57 @@
+package nocache_test
+
+import (
+	"testing"
+
+	"github.com/hearchco/hearchco/src/cache/nocache"
+)
+
+func TestNew(t *testing.T) {
+	_, err := nocache.New()
+	if err != nil {
+		t.Errorf("error creating nocache: %v", err)
+	}
+}
+
+func TestClose(t *testing.T) {
+	db, err := nocache.New()
+	if err != nil {
+		t.Errorf("error creating nocache: %v", err)
+	}
+	db.Close()
+}
+
+func TestSet(t *testing.T) {
+	db, err := nocache.New()
+	if err != nil {
+		t.Errorf("error creating nocache: %v", err)
+	}
+	defer db.Close()
+
+	err = db.Set("testkey", "testvalue")
+	if err != nil {
+		t.Errorf("error setting key-value pair: %v", err)
+	}
+}
+
+func TestGet(t *testing.T) {
+	db, err := nocache.New()
+	if err != nil {
+		t.Errorf("error creating nocache: %v", err)
+	}
+	defer db.Close()
+
+	err = db.Set("testkey", "testvalue")
+	if err != nil {
+		t.Errorf("error setting key-value pair: %v", err)
+	}
+
+	var value string = "testvalue"
+	err = db.Get("testkey", &value)
+	if err != nil {
+		t.Errorf("error getting value: %v", err)
+	}
+	if value != "testvalue" {
+		t.Errorf("expected value: testvalue, got: %v", value)
+	}
+}
