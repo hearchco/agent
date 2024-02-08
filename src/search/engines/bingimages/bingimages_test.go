95ce00e5df2242ac54cc3885fa43d51f7bd29f49
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2024-02-08 16:57:20 +0100

    chore(tests): added bingimages test

--- src/search/engines/bingimages/bingimages_test.go
+++ src/search/engines/bingimages/bingimages_test.go
@@ -1,1 +1,44 @@
 package bingimages_test
+
+import (
+	"testing"
+
+	"github.com/hearchco/hearchco/src/search/engines"
+	"github.com/hearchco/hearchco/src/search/engines/_engines_test"
+)
+
+func TestSearch(t *testing.T) {
+	engineName := engines.BINGIMAGES
+
+	// testing config
+	conf := _engines_test.NewConfig(engineName)
+
+	// test cases
+	tchar := [...]_engines_test.TestCaseHasAnyResults{{
+		Query: "ping",
+		Options: engines.Options{
+			MaxPages:   1,
+			VisitPages: false,
+		},
+	}}
+
+	tccr := [...]_engines_test.TestCaseContainsResults{{
+		Query:     "google",
+		ResultURL: []string{"pngimg.com"},
+		Options: engines.Options{
+			MaxPages:   1,
+			VisitPages: false,
+		},
+	}}
+
+	tcrr := [...]_engines_test.TestCaseRankedResults{{
+		Query:     "wikipedia",
+		ResultURL: []string{"wikipedia."},
+		Options: engines.Options{
+			MaxPages:   1,
+			VisitPages: false,
+		},
+	}}
+
+	_engines_test.CheckTestCases(tchar[:], tccr[:], tcrr[:], t, conf)
+}
