864e30fd50f4681ef5aeb15b752cff3cf6e267c1
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2024-02-23 21:15:14 +0100

    refactor(router): better naming for router and config

--- src/router/router.go
+++ src/router/router.go
@@ -18,11 +18,11 @@ import (
 )
 
 type RouterWrapper struct {
-	router *graceful.Graceful
-	config config.Config
+	rtr  *graceful.Graceful
+	conf config.Config
 }
 
-func New(config config.Config, verbosity int8, lgr zerolog.Logger) (RouterWrapper, error) {
+func New(conf config.Config, verbosity int8, lgr zerolog.Logger) (RouterWrapper, error) {
 	// set verbosity to release mode if log level is INFO
 	if verbosity == 0 {
 		gin.SetMode(gin.ReleaseMode)
@@ -42,10 +42,10 @@ func New(config config.Config, verbosity int8, lgr zerolog.Logger) (RouterWrappe
 
 	// add CORS middleware
 	log.Debug().
-		Str("url", config.Server.FrontendUrl).
+		Str("url", conf.Server.FrontendUrl).
 		Msg("Using CORS")
 	gengine.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{config.Server.FrontendUrl},
+		AllowOrigins:     []string{conf.Server.FrontendUrl},
 		AllowMethods:     []string{"HEAD", "GET", "POST"},
 		AllowHeaders:     []string{"Origin", "X-Requested-With", "Content-Length", "Content-Type", "Accept"},
 		AllowCredentials: false,
@@ -53,28 +53,28 @@ func New(config config.Config, verbosity int8, lgr zerolog.Logger) (RouterWrappe
 	}))
 
 	// create new graceful engine with config port
-	router, err := graceful.New(gengine, graceful.WithAddr(":"+strconv.Itoa(config.Server.Port)))
+	rtr, err := graceful.New(gengine, graceful.WithAddr(":"+strconv.Itoa(conf.Server.Port)))
 	if err != nil {
 		log.Error().
 			Err(err).
 			Msg("router.New(): failed creating new graceful router")
 	}
 
-	return RouterWrapper{router: router, config: config}, err
+	return RouterWrapper{rtr: rtr, conf: conf}, err
 }
 
 func (rw RouterWrapper) runWithContext(ctx context.Context) error {
 	log.Info().
-		Int("port", rw.config.Server.Port).
+		Int("port", rw.conf.Server.Port).
 		Msg("Started router")
-	if err := rw.router.RunWithContext(ctx); err != context.Canceled {
+	if err := rw.rtr.RunWithContext(ctx); err != context.Canceled {
 		log.Error().
 			Err(err).
 			Msg("router.runWithContext(): failed starting router")
 		return err
 	} else if err != nil {
 		log.Info().Msg("Stopping router...")
-		rw.router.Close()
+		rw.rtr.Close()
 		log.Debug().Msg("Successfully stopped router")
 	}
 	return nil
@@ -82,25 +82,25 @@ func (rw RouterWrapper) runWithContext(ctx context.Context) error {
 
 func (rw RouterWrapper) Start(ctx context.Context, db cache.DB, serveProfiler bool) error {
 	// health(z)
-	rw.router.GET("/health", HealthCheck)
-	rw.router.GET("/healthz", HealthCheck)
+	rw.rtr.GET("/health", HealthCheck)
+	rw.rtr.GET("/healthz", HealthCheck)
 
 	// search
-	rw.router.GET("/search", func(c *gin.Context) {
-		err := Search(c, rw.config, db)
+	rw.rtr.GET("/search", func(c *gin.Context) {
+		err := Search(c, rw.conf, db)
 		if err != nil {
 			log.Error().Err(err).Msg("router.Start() (.GET): failed search")
 		}
 	})
-	rw.router.POST("/search", func(c *gin.Context) {
-		err := Search(c, rw.config, db)
+	rw.rtr.POST("/search", func(c *gin.Context) {
+		err := Search(c, rw.conf, db)
 		if err != nil {
 			log.Error().Err(err).Msg("router.Start() (.POST): failed search")
 		}
 	})
 
 	if serveProfiler {
-		pprof.Register(rw.router.Engine)
+		pprof.Register(rw.rtr.Engine)
 	}
 	return rw.runWithContext(ctx)
 }
