b548a96e20b002f5a722fbe7d951ddbedc8196b8
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2024-03-13 14:15:16 +0100

    fix(result): proper decisions when N is less than 4 and more unit tests

--- src/search/result/shorten.go
+++ src/search/result/shorten.go
@@ -2,7 +2,7 @@ package result
 
 func FirstNchars(str string, n int) string {
 	v := []rune(str)
-	if n >= len(v) {
+	if n < 0 || n >= len(v) {
 		return str
 	}
 	return string(v[:n])
@@ -11,16 +11,19 @@ func FirstNchars(str string, n int) string {
 // modifies the passed slice of results,
 // changes the description of the results to be at most N characters long
 func Shorten(results []Result, n int) {
-	if n-3 < 0 {
+	suffix := "..."
+	if n < 0 {
 		return
+	} else if n-len(suffix) <= 0 {
+		suffix = "" // no room for suffix
 	}
 
 	// can't use _, result := range short because we need to modify the elements in slice
 	for i := range results {
 		result := &results[i]
 		if len(result.Description) > n {
-			descShort := FirstNchars(result.Description, n-3)
-			result.Description = descShort + "..."
+			descShort := FirstNchars(result.Description, n-len(suffix))
+			result.Description = descShort + suffix
 		}
 	}
 }
