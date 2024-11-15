a658cd078a7ef54ceea1d3e9b130cc263bb6b347
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2024-11-15 13:38:33 +0100

    refactor(config): disabled engines example config

--- src/config/structs_engines.go
+++ src/config/structs_engines.go
@@ -7,9 +7,9 @@ import (
 // ReaderEngineConfig is format in which the config is read from the config file and environment variables.
 // Used to disable certain search types for an engine. By default, all types are enabled.
 type ReaderEngineConfig struct {
-	NoWeb         bool // Whether this engine is disallowed to do web searches.
-	NoImages      bool // Whether this engine is disallowed to do image searches.
-	NoSuggestions bool // Whether this engine is disallowed to do suggestion searches.
+	NoWeb         bool `koanf:"noweb"`         // Whether this engine is disallowed to do web searches.
+	NoImages      bool `koanf:"noimages"`      // Whether this engine is disallowed to do image searches.
+	NoSuggestions bool `koanf:"nosuggestions"` // Whether this engine is disallowed to do suggestion searches.
 }
 
 // Slices of disabled engines for each search type, by default these are empty.
